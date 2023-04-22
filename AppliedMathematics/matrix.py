c83a34761000d6612ee8e88d374e317b81ede83a
Harsirat Singh Jouhal <98694649+HarsiratSinghJouhal@users.noreply.github.com>
2023-04-22 22:27:43 +0530

    Data __mul__ prototype

--- AppliedMathematics/matrix.py
+++ AppliedMathematics/matrix.py
@@ -10,7 +10,27 @@ class matrix():
         if len(data) == self.elements:
             for i in range(0, self.rows, 1):
                 self.data.append(list(data[i*self.columns : (i + 1) * self.columns]))
+    
+    def __repr__(self) -> str:
+        return self.data
 
+    def __sizeof__(self) -> int:
+        return self.elements
+    
+    def __mul__(self, other):
+        if type(other) == int:
+            return str(self.data) + str(other)
+        elif len(other.data) == len(other.data[0]):
+            count = 0
+
+            for row in self.data:
+                for i in range(len(row)):
+                    row[i] * other.data
+                    
+                
+
+
+        
     def print_m(self):
         print(self.data)
 
@@ -20,3 +40,4 @@ A = matrix(3,3)
 A.add_data(["513874984", 123, 123, 123, 123, 123,123 ,123, "123"])
 
 A.print_m()
+print(A * 5)
