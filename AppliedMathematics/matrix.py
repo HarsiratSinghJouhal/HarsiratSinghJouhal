a090ed9ddabb5030bf2d41f955048f6d870751d1
Harsirat Singh Jouhal <98694649+HarsiratSinghJouhal@users.noreply.github.com>
2023-04-22 23:03:52 +0530

    __mul__ prototype updated

--- AppliedMathematics/matrix.py
+++ AppliedMathematics/matrix.py
@@ -20,24 +20,32 @@ class matrix():
     def __mul__(self, other):
         if type(other) == int:
             return str(self.data) + str(other)
-        elif len(other.data) == len(other.data[0]):
+        elif self.columns == other.rows:
             count = 0
+            res = []
+            while (other.columns - count) != 0:
+                sum = 0
+                for row in self.data:
+                    for i in range(self.columns):
+                        sum += row[i] * other.data[i][other.columns - count - 1]
+                res.append(sum)
 
-            for row in self.data:
-                for i in range(len(row)):
-                    row[i] * other.data
-                    
-                
+                count += 1
+
+            return res
+
 
 
         
     def print_m(self):
         print(self.data)
 
+A = matrix(3,3)
+A.add_data([1,2,3,4,5,6,7,8,9])
+B = matrix(3,3)
+B.add_data([2,2,2,2,2,2,2,2,2])
+A.print_m()
 
 #Testing
-A = matrix(3,3)
-A.add_data(["513874984", 123, 123, 123, 123, 123,123 ,123, "123"])
-
-A.print_m()
-print(A * 5)
+while True:
+    exec(input(">>> "))
